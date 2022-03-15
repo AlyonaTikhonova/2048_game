390ec73d1f36aa62c7e1a2c5957f00d9f2d8a7ee
Алёна <alyonatih@mail.ru>
2022-03-15 17:51:38 +0700

    create classes; add listener for press

--- script.js
+++ script.js
@@ -1,5 +1,113 @@
 "use strict"
 
+document.addEventListener('keyup', clickControl);
+
+function clickControl(event) {
+    if(event.key === "ArrowUp") {
+        console.log('вверх');
+    } else if(event.key === "ArrowDown") {
+        console.log('вниз');
+    } else if(event.key === "ArrowLeft") {
+        console.log('влево');
+    } else if(event.key === "ArrowRight") {
+        console.log('вправо');
+    } else {
+        console.log('нажата другая клавиша');
+    }
+}
+
+const scoreDisplay =  document.querySelector('.score');
+const resultDisplay = document.querySelector('.result');
+const colorCell = [
+    '#afa192',
+    '#eee4da',
+    '#ede0c8',
+    '#f2b179',
+    '#ffcea4',
+    '#e8c064',
+    '#ffab6e',
+    '#fd9982',
+    '#ead79c',
+    '#76daff',
+    '#beeaa5',
+    '#d7d4f0',
+];
+
+class GameManager {
+    constructor() {
+        this.isGameOver = false;
+        this.score = 0;
+        this.board = null;
+    }
+
+    init() {
+        this.board = new Board();
+        document.addEventListener('keyup', clickControl);
+    }
+
+    console.log(checkIsGameOver());
+}
+
+class Board() {
+    constructor() {
+        this.widthBoard = 4;
+        this.squares = [];
+        this.wrapper = document.querySelector('.grid-container');
+    }
+
+    init() {
+        const fragment = document.createDocumentFragment();
+     
+        for (let i = 0; i < this.width * this.width; i++) {
+             const square = document.createElement('div');
+             square.innerHTML = '';
+             square.className = 'grid-cell ';
+     
+             fragment.appendChild(square);
+             squares.push(square);
+         }
+     
+         this.wrapper.appendChild(fragment);
+     }
+
+     console.log(generateNewCell());
+
+     console.log(addColours());
+}
+
+class Cell() {
+    constructor() {
+        this.value = '';
+        this.dom = null;
+    }
+
+    console.log(getValue());
+
+    console.log(setValue());
+    
+    console.log(getNewElement());
+}
+
+const start = new GameManager();
+start.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 шаги:
 1. создаем 16 ячеек, присваиваем каждой значение
